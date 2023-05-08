0db4382ac980aed5b30e8deb70f1096bc9db75ee
Christian Toftgaard Pedersen <85504818+ChristianTP123@users.noreply.github.com>
2023-05-08 07:31:04 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00AADA",
+        main: "#191A19"
+      }
+    },
+  },
+  plugins: [],
+}
+
